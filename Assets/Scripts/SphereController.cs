3e1082936c16441ed91185b4a3b7975ac8f6e850
Fede <ftedinur@itba.edu.ar>
2016-12-17 19:42:55 -0300

    moving sphere

---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+
+    private Rigidbody sphereBody;
+    private Camera sphereCamera;
+
+    public Transform cameraAxis;
+    public float movementSpeed = 1f;
+    public float cameraMoveSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+        sphereBody = GetComponentInChildren<Rigidbody>();
+        sphereCamera = GetComponentInChildren<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hor = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
+
+        if (vert != 0 || hor != 0) {
+            if (Input.GetKey(KeyCode.LeftControl)) {
+                cameraAxis.Rotate(new Vector3(0, hor * cameraMoveSpeed, 0));
+            } else {
+                float moveHorizontal = hor * movementSpeed;
+                float moveVertical = vert * movementSpeed;
+                Vector3 movement = sphereCamera.transform.TransformDirection(new Vector3(moveHorizontal, 0.0f, moveVertical));
+                movement.y = 0;
+                sphereBody.AddForce(movement);
+            }
+        }
+
+        transform.position = sphereBody.transform.position;
+	}
+}
